27ee256392e698e0487e62323e9fd946f9a565f4
gigabite-hub <king46381@gmail.com>
2025-01-21 18:59:44 +0500

    talktwo script added inside body

--- functions.php
+++ functions.php
@@ -459,3 +459,23 @@ function all_available_property($atts) {
     return ob_get_clean();
 }
 add_shortcode('all_available_property', 'all_available_property');
+
+
+function add_talktwo_script_to_body() {
+    ?>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/66435fcd07f59932ab3f67e3/1htrisfl2';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+    <?php
+}
+add_action('wp_body_open', 'add_talktwo_script_to_body');
